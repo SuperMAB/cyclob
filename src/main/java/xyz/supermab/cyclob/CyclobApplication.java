7d542863a2f628da58a577ab2fd74df0afbb7a67
Mohammed Amine BENKHALFA <ambenkhalfa@gmail.com>
2021-10-06 00:03:22 +0100

    First Commit

---
+++ src/main/java/xyz/supermab/cyclob/CyclobApplication.java
@@ -0,0 +1,13 @@
+package xyz.supermab.cyclob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/supermab/cyclob/CyclobApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.supermab.cyclob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
